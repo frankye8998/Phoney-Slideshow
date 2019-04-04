8ab4dcddc72fb4c68e22ba9fb55a30ac1cf0c29e
Frank Ye <frankye8998@gmail.com>
2019-04-04 17:07:28 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-alert("A")
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function title(Stitle) {
+  document.getElementsByTagName("title")[0].innerText = Stitle;
+  if (document.getElementsByTagName("title")[0].innerText == Stitle) {
+    return 0;
+  }
+  return 1;
+}
+
+function redirect(Slocation) { 
+  window.location.href = Slocation;
+}
+
+function background(Scolor) {
+  document.body.style.backgroundColor = Scolor;
+  if(document.body.style.backgroundColor != Scolor) {
+    return 1;
+  }
+  return 0;
+}
+
+function main() {
+
+
+Ssettings = httpGet("settings.txt").split(/\r?\n/);
+for(var i = 0; i < Ssettings.length; i++) {
+  arguments = Ssettings[i].split(" ");
+  switch (arguments[0]) {
+    case "title":
+      title(arguments.slice(1));
+      break;
+    case "background-colour":
+      background(arguments.slice(1));
+      break;
+    }
+  }
+}
+
+window.onload = main;
+
+
